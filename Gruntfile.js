5cb0923f5e1a58c4dfa8a2bbc74dbc263dee5886
saphka <alex.loginov@pm.me>
2019-01-13 17:46:32 +0300

    Functionality completed

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function (grunt) {
         localneo: {
             default: {
                 options: {
-                    basepath: '.'
+                    basePath: '.'
                 }
             }
         }
--- tasks/localneo.js
+++ tasks/localneo.js
@@ -1,13 +1,15 @@
 module.exports = function (grunt) {
     "use strict";
 
+    require("dotenv").config();
+
     function rewriteSetCookie(req, res, next) {
         let oldWriteHead = res.writeHead;
         res.writeHead = function () {
-            let cookie = res.getHeader('Set-Cookie');
+            let cookie = res.getHeader("Set-Cookie");
             if (cookie) {
-                res.setHeader('Set-Cookie', cookie.map(function (item) {
-                    return item.replace(/Domain=.*;/g, '');
+                res.setHeader("Set-Cookie", cookie.map(function (item) {
+                    return item.replace(/Domain=.*;/g, "");
                 }));
             }
             oldWriteHead.apply(res, arguments);
@@ -31,12 +33,12 @@ module.exports = function (grunt) {
             case "sapui5":
                 let proxy = {
                     context: route.path,
-                    host: 'sapui5.hana.ondemand.com',
+                    host: "sapui5.hana.ondemand.com",
                     https: true,
                     rewrite: {}
                 };
 
-                proxy.rewrite[route.path] = (options.sapui5 ? "/" + options.sapui5 : '') + route.target.entryPath;
+                proxy.rewrite[route.path] = (options.sapui5 ? "/" + options.sapui5 : "") + route.target.entryPath;
 
                 return proxy;
             default:
@@ -45,9 +47,17 @@ module.exports = function (grunt) {
     }
 
     function mapDestinationToProxy(route, options) {
+        let host = process.env["DEST_" + route.target.name + "_HOST"],
+            user = process.env["DEST_" + route.target.name + "_USER"],
+            password = process.env["DEST_" + route.target.name + "_PASSWORD"];
+
+        if (!host) {
+            return null;
+        }
+
         let proxy = {
             context: route.path,
-            host: '',
+            host: host,
             https: true,
             headers: {},
             rewrite: {}
@@ -55,35 +65,39 @@ module.exports = function (grunt) {
 
         proxy.rewrite[route.path] = route.target.entryPath ? route.target.entryPath : "";
 
+        if (user && password) {
+            proxy.headers.Authorization = "Basic " + Buffer.from(user + ":" + password).toString("base64");
+        }
+
         return proxy;
     }
 
     grunt.loadNpmTasks("grunt-connect-proxy-updated");
     grunt.loadNpmTasks("grunt-contrib-connect");
 
-    grunt.registerMultiTask('localneo', 'Local NEO runtime', function () {
+    grunt.registerMultiTask("localneo", "Local NEO runtime", function () {
 
         let options = this.options({
             port: 62493,
             open: false,
-            basepath: './webapp',
-            index: '',
-            sapui5: '',
+            basePath: "./webapp",
+            index: "",
+            sapUi5: "",
         });
 
         let proxies = [];
-        let neoapp = grunt.file.readJSON('neo-app.json');
+        let neoapp = grunt.file.readJSON("neo-app.json");
 
         if (Array.isArray(neoapp.routes)) {
             proxies = neoapp.routes.map(route => mapRouteToProxy(route, options)).filter(route => !!route);
         }
 
-        grunt.config('connect', {
+        grunt.config("connect", {
             server: {
                 options: {
                     port: options.port,
-                    hostname: 'localhost',
-                    protocol: 'https',
+                    hostname: "localhost",
+                    protocol: "https",
                     keepalive: true,
                     open: options.open,
                     base: [{
@@ -94,14 +108,14 @@ module.exports = function (grunt) {
                     }],
                     middleware: function (connect, options, middleware) {
                         return [
-                            rewriteSetCookie, require('grunt-connect-proxy-updated/lib/utils').proxyRequest].concat(middleware);
+                            rewriteSetCookie, require("grunt-connect-proxy-updated/lib/utils").proxyRequest].concat(middleware);
                     }
                 },
                 proxies: proxies
             }
         });
 
-        grunt.task.run(['configureProxies:server', 'connect:server']);
+        grunt.task.run(["configureProxies:server", "connect:server" ]);
 
     });
 
